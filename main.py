fb402d97ba0676c14d836ed1351239b7c7874e6d
nagyosse <123080572+nagyosse@users.noreply.github.com>
2023-04-18 04:28:26 +0200

    day: 22 Rendel the app

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -17,6 +18,17 @@ with col2:
     content2 = """
     Below you can find some of te apps I have build in Python. Feel free to contact me!
     """
-    st.write(content2)
+st.write(content2)
 
-    print("Hello")
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
